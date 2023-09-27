60d966113a8e00e98c03b344080b13b61edbb782
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-09-27 14:41:04 +0200

    add route api.web

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+//controllers
+use App\Http\Controllers\Api\ProjectController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function(){
+    Route::resource('project', ProjectController::class)->only([
+        'index',
+        'show'
+    ]);
+
 });
