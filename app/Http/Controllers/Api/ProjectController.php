a186c39402d20d94383d923806183cca74734835
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-09-27 21:31:47 +0200

    add pagination, end bonus

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Project;
 use Illuminate\Http\Request;
+//models
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    public function index (){
+    public function index() {
         $success = true;
-        $results = Project::all();
+        $results = Project::paginate(4);
         return response()->json(compact('success', 'results'));
+    }
+
+    public function show(string $id){
+       
+        $results = Project::find($id);
+        $success = false;
+        if($results){
+            $success = true;
+        }else{
+            $results = 'not found';
+        }
+
+        return response()->json(compact('success', 'results'));
+        
     }
 }
